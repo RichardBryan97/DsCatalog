1185d483fc0f4ade7ceb0b15ac721bb2a8b52829
Richard Bryan <r_bryan@live.com>
2022-04-19 10:37:03 -0300

    git commit -m "Auditoria"

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -4,6 +4,7 @@ import net.bytebuddy.dynamic.loading.InjectionClassLoader;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.time.Instant;
 import java.util.Objects;
 
 @Entity
@@ -16,12 +17,26 @@ public class Category implements Serializable {
     private Long id;
     private String name;
 
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant createdAt;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant updatedAT;
+
     public Category() {
     }
 
     public Category(Long id, String name) {
         this.id = id;
         this.name = name;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public Instant getUpdatedAT() {
+        return updatedAT;
     }
 
     public Long getId() {
@@ -38,6 +53,16 @@ public class Category implements Serializable {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @PrePersist
+    private void prePersist(){
+        createdAt = Instant.now();
+    }
+
+    @PreUpdate
+    private void preUpdate(){
+        updatedAT = Instant.now();
     }
 
     @Override
